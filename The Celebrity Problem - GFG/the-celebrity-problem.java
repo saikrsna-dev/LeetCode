683ba3d7e8a6bc61c81cd7847b882cd669a2d017
saikrsna-dev <109585296+saikrsna-dev@users.noreply.github.com>
2023-01-06 19:37:12 +0530

    Added solution - LeetHub

---
+++ The Celebrity Problem - GFG/the-celebrity-problem.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*; 
+
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t>0)
+        {
+            int N = sc.nextInt();
+            int M[][] = new int[N][N];
+            for(int i=0; i<N; i++)
+            {
+                for(int j=0; j<N; j++)
+                {
+                    M[i][j] = sc.nextInt();
+                }
+            }
+            System.out.println(new Solution().celebrity(M,N));
+            t--;
+        }
+    } 
+} 
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution
+{ 
+    //Function to find if there is a celebrity in the party or not.
+    int celebrity(int M[][], int n)
+    {
+    	int a = 0;
+    	int b = n - 1;
+    	while(a < b){
+    	    if(M[a][b] == 1)a++;
+    	    else b--;
+    	}
+    	
+    	for(int i = 0; i < n; i++){
+    	    if((i != a) && (M[a][i] == 1 || M[i][a] == 0))return -1;
+    	}
+    	return a;
+    }
+}
