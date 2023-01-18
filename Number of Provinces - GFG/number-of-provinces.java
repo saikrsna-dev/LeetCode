7c695b1612f6cafec439ced7335238fe0975e0d7
saikrsna-dev <109585296+saikrsna-dev@users.noreply.github.com>
2023-01-18 14:30:05 +0530

    Added solution - LeetHub

---
+++ Number of Provinces - GFG/number-of-provinces.java
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int V = Integer.parseInt(read.readLine());
+            
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            
+            for(int i=0; i<V; i++)
+            {
+                String S[] = read.readLine().split(" ");
+                ArrayList<Integer> temp = new ArrayList<>();
+                for(int j=0; j<V; j++)
+                    temp.add(Integer.parseInt(S[j]));
+                adj.add(temp);
+            }
+
+            Solution ob = new Solution();
+            System.out.println(ob.numProvinces(adj,V));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static void dfs(int src, ArrayList<ArrayList<Integer>> adj, boolean[] visited){
+        visited[src] = true;
+        int vertices = adj.size();
+        for(int i = 0; i < vertices; i++){
+            if(adj.get(src).get(i) == 1 && visited[i] == false){
+                dfs(i, adj, visited);
+            }
+        }
+    }
+    static int numProvinces(ArrayList<ArrayList<Integer>> adj, int vertices) {
+        int count = 0;
+        boolean visited[] = new boolean[vertices];
+        for(int i = 0; i < vertices; i++){
+            if(visited[i] == false){
+                count++;
+                dfs(i, adj, visited);
+            }
+        }
+        return count;
+    }
+};
