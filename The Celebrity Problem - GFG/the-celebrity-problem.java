34efad52b55fe0b9dd888c87b2eed40e98599fc6
saikrsna-dev <109585296+saikrsna-dev@users.noreply.github.com>
2023-02-04 20:51:25 +0530

    Added solution - LeetHub

--- The Celebrity Problem - GFG/the-celebrity-problem.java
+++ The Celebrity Problem - GFG/the-celebrity-problem.java
@@ -35,16 +35,34 @@ class Solution
     //Function to find if there is a celebrity in the party or not.
     int celebrity(int M[][], int n)
     {
-    	int a = 0;
-    	int b = n - 1;
-    	while(a < b){
-    	    if(M[a][b] == 1)a++;
-    	    else b--;
-    	}
+    // 	int a = 0;
+    // 	int b = n - 1;
+    // 	while(a < b){
+    // 	    if(M[a][b] == 1)a++;
+    // 	    else b--;
+    // 	}
     	
-    	for(int i = 0; i < n; i++){
-    	    if((i != a) && (M[a][i] == 1 || M[i][a] == 0))return -1;
-    	}
-    	return a;
+    // 	for(int i = 0; i < n; i++){
+    // 	    if((i != a) && (M[a][i] == 1 || M[i][a] == 0))return -1;
+    // 	}
+    // 	return a;
+    
+    
+    
+        int trusts[] = new int[n ];
+        int trustedBy[] = new int[n];
+        for(int i = 0; i < M.length; i++){
+            for(int j = 0; j < M[i].length; j++){
+                if(M[i][j] == 1){
+                    trusts[i]++;
+                    trustedBy[j]++;
+                }
+            }
+        }
+
+        for(int i = 0; i < n; i++){
+            if(trusts[i] == 0 && trustedBy[i] == n - 1)return i;
+        }
+        return -1;
     }
 }
